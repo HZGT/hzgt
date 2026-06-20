f31de5b1506b133f102cec2c2be44ffddf598bb5
HZGT <1990015622@qq.com>
2026-06-20 13:43:16 +0800

    fix the class about MQTT/MYSQL, the func about set_log

--- start.bat
+++ start.bat
@@ -1,7 +1,3 @@
-git config --global http.proxy http://127.0.0.1:26561
-git config --global https.proxy http://127.0.0.1:26561
-
-
 git add .
 git commit -m "fix the class about MQTT/MYSQL, the func about set_log"
 git push
